09f9026df738adb988a23139316d8c2ef4bc27f5
roadblock37 <Roadblock37@gmail.com>
2023-01-25 12:12:54 -0500

    added basic components images

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect} from 'react';
-import axios from 'axios'
+import logo from "./logo.svg";
+import "./index.css";
+import { useEffect } from "react";
+import axios from "axios";
+import Task from "./Components/task";
 
 function App() {
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios("http://localhost:5000/tasks");
-        console.log(response.data);
-      return response.data;
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchData() 
-  }, [])
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         const response = await axios("http://localhost:5000/tasks");
+  //         console.log(response.data);
+  //         return response.data;
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     };
+  //     fetchData();
+  //   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task">
+      <input type="checkbox"></input>
+      {/*title of the task */}
+      <p>Title</p>
+      {/*button to delete task */}
+      <button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+          <path
+            fill="#494C6B"
+            fillRule="evenodd"
+            d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
